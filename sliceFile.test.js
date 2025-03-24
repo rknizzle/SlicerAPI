a8dd5dbc3fadec2af7a15ea6e58214e61ce7442d
Erik Olson <erikamerico1991@gmail.com>
2025-03-24 00:27:52 -0400

    delete existing gcode file before running test
    -just to be sure the test is always accurate

--- sliceFile.test.js
+++ sliceFile.test.js
@@ -3,8 +3,17 @@ import test from "node:test";
 import assert from "node:assert";
 import path from "node:path";
 import fs from "node:fs";
+import { existsSync, unlinkSync } from "node:fs";
 
 test("slices model into expected gcode and saves to expected output folder", async () => {
+  const existingCubeFile = existsSync("./gcodeFiles/cube.stl.gcode");
+  //if cube.stl.gcode already exists in gcodeFiles, delete it.
+  //make sure the test isn't affected by the file already existing.
+  //not sure if this is necessary.
+  if (existingCubeFile) {
+    unlinkSync("./gcodeFiles/cube.stl.gcode");
+  }
+
   const result = await sliceFile("./test-profile.ini", "./cube.stl");
   const folderPath = "./gcodeFiles";
   const gcodeFiles = fs.readdirSync(folderPath);
