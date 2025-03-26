e3d6119155a0f76c7b8bbf2ce0f737f776c1cd7f
Erik Olson <erikamerico1991@gmail.com>
2025-03-25 23:29:59 -0400

    simplify sliceFile.test.js

--- sliceFile.test.js
+++ sliceFile.test.js
@@ -1,28 +1,25 @@
 import sliceFile from "./sliceFile.js";
 import test from "node:test";
 import assert from "node:assert";
-import path from "node:path";
-import fs from "node:fs";
 import { existsSync, unlinkSync } from "node:fs";
 
 test("slices model into expected gcode and saves to expected output folder", async () => {
   const existingCubeFile = existsSync("./gcodeFiles/cube.stl.gcode");
   //if cube.stl.gcode already exists in gcodeFiles, delete it.
   //make sure the test isn't affected by the file already existing.
-  //not sure if this is necessary.
   if (existingCubeFile) {
     unlinkSync("./gcodeFiles/cube.stl.gcode");
   }
 
   const result = await sliceFile("./test-profile.ini", "./cube.stl");
-  const folderPath = "./gcodeFiles";
-  const gcodeFiles = fs.readdirSync(folderPath);
-  const file = gcodeFiles.find((file) => file === "cube.stl.gcode");
 
-  //path.join removes the leading ./ so I am adding it back
-  const filePath = file ? `./${path.join(folderPath, file)}` : undefined;
+  const expected = "./gcodeFiles/cube.stl.gcode";
+  //result and expected should be matching file paths or the test will fail.
+  assert.strictEqual(result, expected);
 
-  assert.strictEqual(result, filePath);
+  //this is checking if the file exists in the gcodeFiles folder
+  //test fails if the file doesn't exist.
+  assert.ok(existsSync(expected), "Expected G-code file to exist");
 });
 
 test("throws an error when given a broken model file", async () => {
